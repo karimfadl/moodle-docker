4288dcf4ae1fe57d5f70ae86770966d8561e59b0
Karim Fadl <kareemfadl10@gmail.com>
2020-08-10 15:16:22 +0000

    moodle-docker

--- Deploy-app.sh
+++ Deploy-app.sh
@@ -6,55 +6,16 @@ read vardomain
 sed -i 's/moodle.domain.com/$vardomain/g; s/moodle.key/$vardomain.key/g; s/moodle.crt/$vardomain.crt/g' ./config/nginx/app_moodle.conf
 
 # Create Self Signed Certificate for Moodle Domain
-DOMAIN="$vardomain"
-if [ -z "$DOMAIN" ]; then
-  echo "Usage: $(basename $0) <domain>"
-  exit 11
-fi
-
-fail_if_error() {
-  [ $1 != 0 ] && {
-    unset PASSPHRASE
-    exit 10
-  }
-}
-
-# Generate a passphrase
-export PASSPHRASE=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 128; echo)
-
 subj="
 C=EG
 ST=Cairo
-O=karimlabs
 localityName=Sohag
-commonName=$DOMAIN
+O=karimlabs
 organizationalUnitName=IT
-emailAddress=karim.fadl@$DOMAIN
+commonName=$vardomain
+emailAddress=karim.fadl@$vardomain
 "
-
-# Generate the server private key
-openssl genrsa -des3 -out ./config/ssl/$DOMAIN.key -passout env:PASSPHRASE 2048
-fail_if_error $?
-
-# Generate the CSR
-openssl req \
-    -new \
-    -batch \
-    -subj "$(echo -n "$subj" | tr "\n" "/")" \
-    -key ./config/ssl/$DOMAIN.key \
-    -out ./config/ssl/$DOMAIN.csr \
-    -passin env:PASSPHRASE
-fail_if_error $?
-cp ./config/ssl/$DOMAIN.key ./config/ssl/$DOMAIN.key.org
-fail_if_error $?
-
-# Strip the password so we don't have to type it every time we restart Apache
-openssl rsa -in ./config/ssl/$DOMAIN.key.org -out ./config/ssl/$DOMAIN.key -passin env:PASSPHRASE
-fail_if_error $?
-
-# Generate the cert (good for 10 years)
-openssl x509 -req -days 3650 -in ./config/ssl/$DOMAIN.csr -signkey ./config/ssl/$DOMAIN.key -out $DOMAIN.crt
-fail_if_error $?
+openssl req -new -x509 -days 365 -nodes -out ./config/ssl/$vardomain.crt -keyout ./config/ssl/$vardomain.key
 
 # Download Moodle LMS Files
 wget https://download.moodle.org/download.php/direct/stable39/moodle-3.9.1.tgz
